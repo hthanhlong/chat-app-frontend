11c1c6f1e8642a46be9554894eeae8db4196dba5
LongHoang <longhoang@gmail.com>
2024-03-19 14:56:37 -0700

    change title button

--- src/pages/settings/components/FriendRequest.tsx
+++ src/pages/settings/components/FriendRequest.tsx
@@ -6,257 +6,353 @@ const FriendRequest = () => {
     <div className="flex flex-wrap h-[980px] overflow-auto">
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
       <div className="border-[1px] w-[calc(50%-8px)] m-1 rounded-md py-1 px-2 flex items-center justify-between hover:bg-gray-100">
         <Avatar name="Brett Johnson" textSize="md" />
-        <div>
-          <Button color="blue" size="sm">
+        <div className="flex">
+          <Button color="blue" size="sm" className="mr-1">
             Accept
+          </Button>
+          <Button color="red" size="sm">
+            Reject
           </Button>
         </div>
       </div>
