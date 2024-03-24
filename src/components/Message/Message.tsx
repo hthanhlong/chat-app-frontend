d0901731e94f279b163229e45461da472ee8e192
LongHoang <longhoang@gmail.com>
2024-03-23 19:55:59 -0700

    add dark mode continue

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -5,11 +5,11 @@ const Message = ({
   isSender?: boolean
   message: string
 }) => {
-  const classSender = isSender ? "ml-auto bg-blue-200" : "bg-white"
+  const classSender = isSender ? "ml-auto mr-2" : ""
 
   return (
     <div
-      className={`pb-2 max-w-[460px] w-fit rounded-md my-1 p-2 ${classSender}`}
+      className={`pb-2 max-w-[460px] w-fit rounded-md my-1 p-2 ${classSender} dark:bg-slate-800 dark:text-white `}
     >
       {message}
     </div>
