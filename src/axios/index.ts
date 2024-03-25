360a3a1b9d1f8c6aac7950ef02043057d346b1fd
LongHoang <longhoang@gmail.com>
2024-03-25 14:47:57 -0700

    handle refresh token

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,6 +2,8 @@ import axios from "axios"
 import { refreshToken } from "./auth"
 import { AUTH_VARIABLE } from "../constant"
 
+let isCalling = false
+
 export const http = axios.create({
   baseURL: "http://localhost:8080/api/v1/",
 })
@@ -23,7 +25,11 @@ http.interceptors.response.use(
   (response) => response.data,
   async (error) => {
     const originalRequest = error.config
-    if (error.response?.data.errorCode === "AccessTokenExpiredError") {
+    if (
+      error.response?.data.errorCode === "AccessTokenExpiredError" &&
+      isCalling === false
+    ) {
+      isCalling = true
       originalRequest._retry = true
       try {
         const newToken = await refreshToken()
@@ -31,7 +37,18 @@ http.interceptors.response.use(
         const { accessToken } = newToken
         originalRequest.headers["Authorization"] = "Bearer " + accessToken
         localStorage.setItem(AUTH_VARIABLE.ACCESS_TOKEN, accessToken)
+        setTimeout(() => (isCalling = false), 3000)
         return http(originalRequest)
+      } catch (refreshError) {
+        console.error(refreshError)
+        return Promise.reject(refreshError)
+      }
+    }
+
+    if (error.response?.data.errorCode === "RefreshTokenExpiredError") {
+      try {
+        localStorage.clear()
+        window.location.reload()
       } catch (refreshError) {
         console.error(refreshError)
         return Promise.reject(refreshError)
