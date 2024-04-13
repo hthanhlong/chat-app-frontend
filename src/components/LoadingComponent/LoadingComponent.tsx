58310358a5fad42828c57a24b19fc01a0cde1730
LongHoang <longhoang@gmail.com>
2024-04-12 22:43:50 -0700

    fix loading component

--- src/components/LoadingComponent/LoadingComponent.tsx
+++ src/components/LoadingComponent/LoadingComponent.tsx
@@ -6,13 +6,10 @@ const LoadingComponent = () => {
 
   return (
     <>
-      {isLoading ? (
-        <>
-          <div className="loader absolute z-10"></div>
-          <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-blue-200 opacity-30"></div>
-        </>
-      ) : (
-        <></>
+      {isLoading && (
+        <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-blue-200 bg-opacity-30">
+          <div className="loader"></div>
+        </div>
       )}
     </>
   )
