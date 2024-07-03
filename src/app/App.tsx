5cb50eaa0277cf467b070ad9713a050f32eb262c
hthanhlong <hthanhlong97@gmail.com>
2024-07-03 13:04:09 -0700

    chore: remove emptly lines

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from 'react-router-dom'
-
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { AnimatePresence } from 'framer-motion'
